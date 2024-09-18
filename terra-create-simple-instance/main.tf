111f7f75ce5e6c76474a531898eaa7e0cf8f9eba
sky-lester <lesterreandino@skydev.solutions>
2024-09-18 14:15:22 +0800

    create test instance with security

--- terra-create-simple-instance/main.tf
+++ terra-create-simple-instance/main.tf
@@ -2,10 +2,45 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "test-instance" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
+resource "aws_security_group" "terra_sg" {
+  name        = "terra_sg"
+  description = "Terraform Security group for EC2 Instances"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
-    Name = "EdgeInstance-terra"
+    Name = "terra-sg"
   }
 }
+
+resource "aws_instance" "test_instance" {
+  ami             = "ami-01811d4912b4ccb26" # ubuntu ami
+  instance_type   = "t2.micro"
+  key_name        = "llr-keypair"
+  depends_on      = [aws_security_group.terra_sg]
+  vpc_security_group_ids = [aws_security_group.terra_sg.id]
+  # security_groups = [aws_security_group.terra_sg.name]
+  tags = {
+    Name = "terra-instance"
+  }
+}
+
