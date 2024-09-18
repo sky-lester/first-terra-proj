b5cafe0614470e3b6dc8f578fea02c51bd566b43
sky-lester <lesterreandino@skydev.solutions>
2024-09-18 09:16:08 +0800

    add health checks

--- terra-create-vpc-alb-asg/alb.tf
+++ terra-create-vpc-alb-asg/alb.tf
@@ -15,6 +15,15 @@ resource "aws_lb_target_group" "alb_ec2_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.custom_vpc.id
 
+   health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
   tags = {
     Name = "alb_ec2_tg"
   }
